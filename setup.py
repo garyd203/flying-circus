4a70232dee468bd913b0a1f06a31d4b5cbc86b9a
Gary Donovan <gazza@gazza.id.au>
2017-09-14 08:22:08 +1000

    #1: Create setup.py for initial upload to PyPI

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flying-circus",
+    version="0.0.1",
+    packages=find_packages(),
+
+    # metadata for upload to PyPI
+    author="Gary Donovan",
+    author_email="gazza@gazza.id.au",
+    description="A tool for managing AWS infrastructure.",
+    license="LGPL", #TODO is this correct? v3?
+    keywords="cloud formation",
+    url="https://github.com/garyd203/flying-circus",
+    classifiers=[
+        # TODO add more classifiers
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
