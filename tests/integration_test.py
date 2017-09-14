baf8b2c7ccd4ac99ec54a0a5138da810798fac2d
Gary Donovan <gazza@gazza.id.au>
2017-09-14 23:16:49 +1000

    #2: Layout API using some tests against the sample S3.

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,12 +1,7 @@
 from flyingcircus.core import Stack
 from flyingcircus.services import s3
 
-
-class TestCoreServices:
-    def test_simple_s3(self):
-        """As a developer, I can create objects to represent a simple S3 bucket."""
-        # Setup
-        expected_yaml = '''
+SIMPLE_S3_YAML = '''
 {
   "AWSTemplateFormatVersion" : "2010-09-09",
 
@@ -36,11 +31,67 @@ class TestCoreServices:
       "Description" : "Name of S3 bucket to hold website content"
     }
   } 
-}'''
+}'''  # FIXME this is JSON, not YAML
 
+
+class TestCoreServices:
+    def test_s3_as_naive_python(self):
+        """As a developer, I can use a naive python representation of YAML to create objects representing a basic S3 bucket."""
         # Exercise
-        stack = Stack()
-        stack.add(s3.Bucket())
+        stack = Stack(
+            AWSTemplateFormatVersion="2010-09-09",
+            Description="vcvxcv",
+        )
+        stack.Resources["S3Bucket"] = s3.Bucket(
+            Properties={
+                "AccessControl": "PublicRead",
+                "WebsiteConfiguration": {
+                    "IndexDocument": "index.html",
+                    "ErrorDocument": "error.html",
+                }
+            },
+            DeletionPolicy="Retain",
+        )
+        stack.Outputs["WebsiteURL"] = Output(
+            "!GetAtt S3Bucket.WebsiteURL",
+            Description="URL for website hosted on S3",
+        )
+        stack.Outputs["S3BucketSecureURL"] = Output(
+            '''!Join ["", ["https://", !GetAtt S3Bucket.DomainName]]''',
+            Description="Name of S3 bucket to hold website content",
+        )
 
         # Verify
-        assert expected_yaml == stack.export()
+        assert stack.export() == SIMPLE_S3_YAML
+
+    def test_s3_as_simple_python(self):
+        """As a developer, I can use a simplified Python interface to create Python objects representing a basic S3 bucket."""
+        # Exercise
+        stack = Stack(
+            Description="vcvxcv",
+        )
+        bucket = s3.Bucket(
+            Name="S3Bucket",
+            Properties={
+                "AccessControl": "PublicRead",
+                "WebsiteConfiguration": {
+                    "IndexDocument": "index.html",
+                    "ErrorDocument": "error.html",
+                }
+            },
+            DeletionPolicy="Retain",
+        )
+        stack.add(bucket)
+        stack.add(Output(
+            Name="WebsiteURL",
+            Value=bucket.WebsiteURL,
+            Description="URL for website hosted on S3",
+        ))
+        stack.add(Output(
+            Name="S3BucketSecureURL",
+            Value=YamlJoin("https://", bucket.DomainName),
+            Description="Name of S3 bucket to hold website content",
+        ))
+
+        # Verify
+        assert stack.export() == SIMPLE_S3_YAML
