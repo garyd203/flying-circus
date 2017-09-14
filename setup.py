f562b4202effc4d3be41c53a24b981cabcc7e9b1
Gary Donovan <gazza@gazza.id.au>
2017-09-14 17:20:16 +1000

    #1:  Add version attribute on package. Bump to v0.0.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flying-circus",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
 
     # metadata for upload to PyPI
