5965b99fce5525394038745bac59763ecb71acd1
Gary Donovan <gazza@gazza.id.au>
2017-10-13 08:30:10 +1100

    #14: Fluff around with code structure inside the IAM service module

--- src/flyingcircus/service/iam.py
+++ src/flyingcircus/service/iam.py
@@ -1,27 +1,45 @@
+from .._raw import iam as raw
 from .._raw.iam import *
 
 __all__ = [
-    "AssumeRolePolicy",
     "Policy",
     "PolicyDocument",
     "PolicyStatement",
     "PrincipalSet",
     "Role",
+    "simple_assume_role_policy",
 ]
 
+class PolicyDocument(raw.PolicyDocument):
+    def _override_function_that_updates_parameters_with_default_values(self):
+        pass
 
-class AssumeRolePolicy(PolicyDocument):
-    """A simple trust policy to describe who can assume a role."""
 
-    def __init__(self, principals):
-        PolicyDocument.__init__(
-            self,
-            Version='2012-10-17',
-            Statement=[
-                PolicyStatement(
-                    Action=['sts:AssumeRole'],
-                    Effect="Allow",
-                    Principal=principals,
-                ),
-            ],
-        )
+class Defaults:
+    """Standard implementation of all raw classes with sensible defaults"""
+
+    @staticmethod
+    def PolicyDocument(Version="2012-10-17",
+                       **kwargs):  # TODO want all arguments to be present here for autocomplete and typing
+        # kwargs.setdefault("Version", "2012-10-17")
+        return raw.PolicyDocument(**kwargs)
+
+
+class BestPractice:
+    """Best-practice implementations for basic scenarios, cookbook-style"""
+    # TODO advanced scenarios should be in another package, maybe?
+    pass
+
+
+def simple_assume_role_policy(principals):
+    """Create a simple trust policy document to describe who can assume a role."""
+    return PolicyDocument(
+        Version='2012-10-17',
+        Statement=[
+            PolicyStatement(
+                Action=['sts:AssumeRole'],
+                Effect="Allow",
+                Principal=principals,
+            ),
+        ],
+    )
