f730d8c2643ec74e1782ea096483f20da410c5fe
Gary Donovan <gazza@gazza.id.au>
2017-12-13 08:44:15 +1100

    #59: Test helper to call `aws cloudformation validate` with boto

---
+++ tests/validation_helper.py
@@ -0,0 +1,31 @@
+"""Helper functions to validate output."""
+
+import boto3
+import pytest
+from botocore.exceptions import ClientError
+
+
+class AwsTemplateValidation:
+    """Base class to validate the content of a CloudFormation stack template."""
+
+    def get_stacks_under_test(self):
+        """Return a list of concrete stack objects that need to be validated."""
+        return []
+
+    def test_validate_stacks(self):
+        # TODO add pytest config and conditional skip so that this only runs if you want it to
+        # Setup
+        cfclient = boto3.client("cloudformation")
+
+        # Verify
+        for stack in self.get_stacks_under_test():
+            template = stack.export("yaml")
+            try:
+                cfclient.validate_template(TemplateBody=template)
+            except ClientError as ex:
+                print("Error validating template for `{}`".format(stack.Description.replace('\n', ' ')))
+                error_data = getattr(ex, "response", {}).get("Error", None)
+                if error_data and error_data["Code"] == "ValidationError":
+                    pytest.fail("{} template is invalid: {}".format(stack.__class__.__name__, error_data["Message"], ))
+                else:
+                    raise
