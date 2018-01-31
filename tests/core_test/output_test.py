a0eb25237f40bb6c7b96d561f46cfc723e6b8ade
Gary Donovan <gazza@gazza.id.au>
2018-02-01 10:11:42 +1100

    #35: Add class for modelling a stack Output

---
+++ tests/core_test/output_test.py
@@ -0,0 +1,21 @@
+"""Tests for Parameters and PseudoParameters"""
+
+from flyingcircus.core import Output
+from flyingcircus.core import dedent
+
+
+class TestBasicOutputBehaviour:
+    """Verify basic behaviour of the Output class"""
+
+    def test_export_basic_output(self):
+        """Should be able to create and export a simple output object."""
+        output = Output(Description="Stuff we need", Value=6644, Export={"Name": "ImportantThingy"})
+        exported = output.export("yaml")
+
+        assert exported == dedent("""
+        ---
+        Description: Stuff we need
+        Export:
+          Name: ImportantThingy
+        Value: 6644
+        """)
