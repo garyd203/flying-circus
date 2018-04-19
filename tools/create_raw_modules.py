7da63676a2e9998d501f894e8d4833aa20668e8f
Gary Donovan <gary.donovan@rockmelon.co>
2018-04-20 08:11:32 +1000

    #15: Convert code generator to use "click"

--- tools/create_raw_modules.py
+++ tools/create_raw_modules.py
@@ -5,13 +5,15 @@ Create the basic Python modules in the `_raw` package.
 
 These are automatically generated from a JSON specification provided by Amazon.
 """
+
 # TODO reference the AWS documentation at https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-resource-specification-format.html
+# TODO generate the modules and commit them
 
 import json
 import logging
 import os.path
-import sys
 
+import click
 import inflection
 from jinja2 import FileSystemLoader, Environment
 
@@ -23,6 +25,7 @@ LOGGER = logging.getLogger(
 #: At the moment we only include some modules because we are still building
 #: up our script
 SUPPORTED_SERVICES = {
+    # FIXME remove this temporary hack when we are satisfied wth the output
     "AutoScaling",
 }
 
@@ -30,18 +33,44 @@ SUPPORTED_SERVICES = {
 #: not appear to be in the specification, and is not always in a predictable
 #: place
 SERVICE_DOCUMENTATION_URL = {
+    # FIXME add more to here. log warning if a module is missing
     "AutoScaling": "http://docs.aws.amazon.com/autoscaling/latest/userguide/WhatIsAutoScaling.html",
 }
 
+#: The directory where this script lives
+SCRIPTDIR = os.path.dirname(__file__)
 
-def parse_specification(specfile, outputdir):
+
+@click.command()
+@click.argument("packagedir",
+                metavar="FCPACKAGE",
+                type=click.Path(exists=True, file_okay=False),
+                required=True,
+                )
+@click.option("--specification", "-s",
+              type=click.Path(exists=True, dir_okay=False),
+              default=os.path.join(
+                  SCRIPTDIR, "..", "contrib", "CloudFormationResourceSpecification.json"
+              ),
+              help="JSON resource specification from Amazon Web Services.",
+              show_default=True,
+              )
+def generate_raw_package(packagedir, specification):
+    """Create the basic Python modules in the `_raw` package.
+
+    The output directory argument should be the location of the existing
+    'src/flyingcircus/' directory, where generated files will be placed.
+    Existing files will be replaced.
+    """
     # Check inputs
-    os.makedirs(outputdir, exist_ok=True)
+    raw_dirname = os.path.join(packagedir, "_raw")
+    assert os.path.isdir(raw_dirname), "The 'flyingcircus._raw' package does not exist"
 
-    # TODO Ensure the directory is a Python package
+    service_dirname = os.path.join(packagedir, "service")
+    assert os.path.isdir(service_dirname), "The 'flyingcircus.service' package does not exist"
 
     # Load data as a python dictionary
-    with open(specfile, "r") as fp:
+    with open(specification, "r") as fp:
         all_data = json.load(fp)
 
     assert set(all_data.keys()) == {"PropertyTypes", "ResourceSpecificationVersion", "ResourceTypes"}, \
@@ -87,22 +116,21 @@ def parse_specification(specfile, outputdir):
     # Create a Python module for each AWS service, with a Python class for
     # each AWS resource type
     env = Environment(
-        loader=FileSystemLoader("."),
+        loader=FileSystemLoader(SCRIPTDIR),
     )
     template = env.get_template('raw_module.py.jinja2')
 
     for service_name, service in sorted(services.items()):
-        module_name = os.path.join(outputdir, service_name.lower() + ".py")
+        module_name = os.path.join(raw_dirname, service_name.lower() + ".py")
         with open(module_name, "w") as fp:
+            LOGGER.debug("Generating module %s.py", service_name.lower())
             fp.write(template.render(service=service))
+
+        # TODO check that there is a basic module in the `service` package
 
 
 if __name__ == '__main__':
-    # TODO use a real arg parser
     logging.basicConfig()
-    logging.getLogger("").setLevel(logging.INFO)
+    LOGGER.setLevel(logging.DEBUG)
 
-    specfile = sys.argv[1]
-    outputdir = sys.argv[2]
-
-    parse_specification(specfile, outputdir)
+    generate_raw_package()
