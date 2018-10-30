c413cf9f5724dd3b6e30240e44eec5f45b0e5d30
Gary Donovan <gary.donovan@rockmelon.co>
2018-10-30 21:23:09 +1100

    #113: Don't let a KMS Key have it's nae set as a tag

--- src/flyingcircus/service/kms.py
+++ src/flyingcircus/service/kms.py
@@ -5,8 +5,14 @@ See Also:
     <https://docs.aws.amazon.com/kms/latest/developerguide/overview.html>`_
 """
 
-# noinspection PyUnresolvedReferences
 from .._raw import kms as _raw
 
 # noinspection PyUnresolvedReferences
 from .._raw.kms import *
+
+
+class Key(_raw.Key):
+    @property
+    def name(self):
+        # TODO #113 create an Alias instead
+        raise NotImplementedError("Key.name cannot be set as a tag. We need to use an Alias.")
