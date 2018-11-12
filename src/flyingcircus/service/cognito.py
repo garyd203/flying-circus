f68f713277eb8815e9592187f616d018bd66ef02
Gary Donovan <gary.donovan@rockmelon.co>
2018-11-12 16:08:52 +1100

    Create raw and service files for Cognito service

---
+++ src/flyingcircus/service/cognito.py
@@ -0,0 +1,12 @@
+"""General-use classes to interact with the Cognito service through CloudFormation.
+
+See Also:
+    `AWS developer guide for Cognito
+    <https://docs.aws.amazon.com/cognito/latest/developerguide/index.html>`_
+"""
+
+# noinspection PyUnresolvedReferences
+from .._raw import cognito as _raw
+
+# noinspection PyUnresolvedReferences
+from .._raw.cognito import *
