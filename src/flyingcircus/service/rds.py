8d657489081d240f5747445eb285d5d02e51ea75
Gary Donovan <gary.donovan@rockmelon.co>
2018-11-21 10:44:23 +1100

    Get DBInstance name from DBInstanceIdentifier property

--- src/flyingcircus/service/rds.py
+++ src/flyingcircus/service/rds.py
@@ -5,8 +5,23 @@ See Also:
     <https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/index.html>`_
 """
 
-# noinspection PyUnresolvedReferences
 from .._raw import rds as _raw
 
 # noinspection PyUnresolvedReferences
 from .._raw.rds import *
+
+
+class DBInstance(_raw.DBInstance):
+    # Implement Naming
+    # ----------------
+    @property
+    def name(self):
+        try:
+            return self.Properties["DBInstanceIdentifier"]
+        except KeyError:
+            # No name set yet
+            return None
+
+    @name.setter
+    def name(self, value: str):
+        self.Properties["DBInstanceIdentifier"] = value
