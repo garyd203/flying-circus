13998f9f35b76962f08f9f123061a4b252ed9d43
Gary Donovan <gary.donovan@rockmelon.co>
2018-11-21 15:42:42 +1100

    Get Secret name from Name property

--- src/flyingcircus/service/secretsmanager.py
+++ src/flyingcircus/service/secretsmanager.py
@@ -5,8 +5,23 @@ See Also:
     <https://docs.aws.amazon.com/secretsmanager/latest/userguide/intro.html>`_
 """
 
-# noinspection PyUnresolvedReferences
 from .._raw import secretsmanager as _raw
 
 # noinspection PyUnresolvedReferences
 from .._raw.secretsmanager import *
+
+
+class Secret(_raw.Secret):
+    # Implement Naming
+    # ----------------
+    @property
+    def name(self):
+        try:
+            return self.Properties["Name"]
+        except KeyError:
+            # No name set yet
+            return None
+
+    @name.setter
+    def name(self, value: str):
+        self.Properties["Name"] = value
