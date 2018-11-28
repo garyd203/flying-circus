028788aeda31aef95197fd4afc28901a42a3b4e2
Gary Donovan <gazza@gazza.id.au>
2018-11-28 21:39:16 +1100

    Tidy up setup keywords, and ship in README as long description

--- setup.py
+++ setup.py
@@ -3,19 +3,33 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
+THIS_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
+
 
 def get_version_data():
     data = {}
-    with open(os.path.join("src", "flyingcircus", "_about.py")) as fp:
+    with open(os.path.join(THIS_DIRECTORY, "src", "flyingcircus", "_about.py")) as fp:
         exec(fp.read(), data)
     return data
 
 
+def get_readme():
+    with open(os.path.join(THIS_DIRECTORY, "README.md")) as fp:
+        desc = fp.read()
+    return desc
+
+
 setup(
+    # Basic Project Description
     name="flying-circus",
     version=get_version_data()['__version__'],
+    description="A tool for describing AWS infrastructure.",
+    long_description=get_readme(),
+    long_description_content_type='text/markdown',
+
+    # Project Packaging
     package_dir={'': 'src'},
-    packages=find_packages(where="src"),
+    packages=find_packages(where="src"),  # FIXME do we need this?
     install_requires=[
         # We use the `kw_only` only for attribute classes, which was
         # introduced in v18.2.0
@@ -23,18 +37,19 @@ setup(
         'PyYAML',
     ],
 
-    # metadata for upload to PyPI
+    # Contact Details
     author="Gary Donovan",
     author_email="gazza@gazza.id.au",
-    description="A tool for managing AWS infrastructure.",
-    license="LGPL v3",
-    keywords="AWS cloudformation infrastructure-as-code",
     url="https://github.com/garyd203/flying-circus",
     project_urls={
         'Documentation': "https://flying-circus.readthedocs.io/en/latest/",
         'Source': "https://github.com/garyd203/flying-circus",
         'Tracker': "https://github.com/garyd203/flying-circus/issues",
     },
+
+    # Other metadata for PyPI
+    license="LGPL v3",
+    keywords="AWS cloudformation infrastructure-as-code",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -48,4 +63,5 @@ setup(
         'Topic :: Software Development :: Pre-processors',
         'Topic :: System :: Systems Administration',
     ],
+    metadata_version="2.1",
 )
