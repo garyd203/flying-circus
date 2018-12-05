b8c55db1db7a6dae6d405fad13ff707020cddb83
Gary Donovan <gazza@gazza.id.au>
2018-12-06 08:26:22 +1100

    Use an alias for SSM Parameter

--- src/flyingcircus/service/ssm.py
+++ src/flyingcircus/service/ssm.py
@@ -5,8 +5,12 @@ See Also:
     <https://docs.aws.amazon.com/systems-manager/latest/APIReference/Welcome.html>`_
 """
 
-# noinspection PyUnresolvedReferences
 from .._raw import ssm as _raw
 
 # noinspection PyUnresolvedReferences
 from .._raw.ssm import *
+
+# Also provide an alias for the SSM Parameter resource, to reduce conflicts
+# with the top-level template Parameter object
+SSMParameter = _raw.Parameter
+SSMParameterProperties = _raw.ParameterProperties
