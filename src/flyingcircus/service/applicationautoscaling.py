4874b2b76979d7183caa591415fb792eca5c6f15
Gary Donovan <gazza@gazza.id.au>
2018-12-10 09:58:38 +1100

    #15: Generate modules for Application AutoScaling and ALB

---
+++ src/flyingcircus/service/applicationautoscaling.py
@@ -0,0 +1,12 @@
+"""General-use classes to interact with the ApplicationAutoScaling service through CloudFormation.
+
+See Also:
+    `AWS developer guide for ApplicationAutoScaling
+    <https://docs.aws.amazon.com/autoscaling/application/APIReference/Welcome.html>`_
+"""
+
+# noinspection PyUnresolvedReferences
+from .._raw import applicationautoscaling as _raw
+
+# noinspection PyUnresolvedReferences
+from .._raw.applicationautoscaling import *
---
+++ src/flyingcircus/service/elasticloadbalancingv2.py
@@ -0,0 +1,12 @@
+"""General-use classes to interact with the ElasticLoadBalancingV2 service through CloudFormation.
+
+See Also:
+    `AWS developer guide for ElasticLoadBalancingV2
+    <https://docs.aws.amazon.com/elasticloadbalancing/latest/userguide/index.html>`_
+"""
+
+# noinspection PyUnresolvedReferences
+from .._raw import elasticloadbalancingv2 as _raw
+
+# noinspection PyUnresolvedReferences
+from .._raw.elasticloadbalancingv2 import *
