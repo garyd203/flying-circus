7c220aeb28aab8c3b2c8be014d08b1f71e78c86b
Gary Donovan <gazza@gazza.id.au>
2018-12-11 16:19:37 +1100

    #15: Generate modules for ACM

---
+++ src/flyingcircus/service/certificatemanager.py
@@ -0,0 +1,12 @@
+"""General-use classes to interact with the CertificateManager service through CloudFormation.
+
+See Also:
+    `AWS developer guide for CertificateManager
+    <https://docs.aws.amazon.com/acm/latest/userguide/acm-overview.html>`_
+"""
+
+# noinspection PyUnresolvedReferences
+from .._raw import certificatemanager as _raw
+
+# noinspection PyUnresolvedReferences
+from .._raw.certificatemanager import *
