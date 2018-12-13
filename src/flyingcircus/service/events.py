9321667310b2260480a9edaf814331c4714dae24
Gary Donovan <gazza@gazza.id.au>
2018-12-14 09:55:02 +1100

    #15: Generate modules for the CloudWatch Events service

---
+++ src/flyingcircus/service/events.py
@@ -0,0 +1,12 @@
+"""General-use classes to interact with the Events service through CloudFormation.
+
+See Also:
+    `AWS developer guide for Events
+    <https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/WhatIsCloudWatchEvents.html>`_
+"""
+
+# noinspection PyUnresolvedReferences
+from .._raw import events as _raw
+
+# noinspection PyUnresolvedReferences
+from .._raw.events import *
