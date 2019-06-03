eee30983a872c3cdca7d5968cb44df9958c49bda
Gary Donovan <gazza@gazza.id.au>
2019-06-03 12:40:07 +1000

    Helper function to get a Lambda runtime that matches our Python version

--- src/flyingcircus/service/lambda_.py
+++ src/flyingcircus/service/lambda_.py
@@ -5,8 +5,40 @@ See Also:
     <https://docs.aws.amazon.com/lambda/latest/dg/welcome.html>`_
 """
 
+import sys
+
 # noinspection PyUnresolvedReferences
 from .._raw import lambda_ as _raw
 
 # noinspection PyUnresolvedReferences
 from .._raw.lambda_ import *
+
+#: Lambda Runtimes for Python that we know about.
+#: See `the definitive list in the AWS documentation
+#: <https://docs.aws.amazon.com/lambda/latest/dg/python-programming-model.html>`_
+_KNOWN_LAMBDA_RUNTIMES = {(2, 7): "python2.7", (3, 6): "python3.6", (3, 7): "python3.7"}
+
+
+def get_lambda_runtime_for_this_process() -> str:
+    """Determine the best Lambda runtime for the current Python process."""
+    # The basic premise is to use exact matching if possible, upgrade if
+    # necessary, and don't downgrade
+
+    (major, minor, _, _, _) = sys.version_info
+
+    try:
+        return _KNOWN_LAMBDA_RUNTIMES[(major, minor)]
+    except KeyError:
+        pass
+
+    # Maybe this is an old Python v3
+    if major == 3 and minor < 6:
+        return _KNOWN_LAMBDA_RUNTIMES[(3, 6)]
+
+    # Maybe this is an old Python v2
+    if major == 2 and minor < 7:
+        return _KNOWN_LAMBDA_RUNTIMES[(2, 7)]
+
+    raise ValueError(
+        "Python version '{}' cannot be supported by AWS Lambda".format(sys.version)
+    )
