2e7e977e3013dd503fc161857d3bbd24c332c9ca
Gary Donovan <gazza@gazza.id.au>
2019-06-03 15:19:19 +1000

    Factory function to create an inline Python Lambda function

--- src/flyingcircus/service/lambda_.py
+++ src/flyingcircus/service/lambda_.py
@@ -5,13 +5,86 @@ See Also:
     <https://docs.aws.amazon.com/lambda/latest/dg/welcome.html>`_
 """
 
+import inspect
 import sys
+import warnings
+from typing import Callable
+
+import inflection
 
 # noinspection PyUnresolvedReferences
 from .._raw import lambda_ as _raw
 
 # noinspection PyUnresolvedReferences
 from .._raw.lambda_ import *
+
+
+class Function(_raw.Function):
+    __slots__ = []
+
+    @classmethod
+    def create_from_python_function(
+        cls, handler: Callable, Role, Description=None, Runtime=None
+    ) -> "Function":
+        """Factory function to create a Function with inlined code from a Python function."""
+        # Extract and format source code
+        handler_sig = inspect.signature(handler)
+        if len(handler_sig.parameters) > 2:
+            raise ValueError("Lambda handler takes too many parameters")
+
+        param_names = list(handler_sig.parameters.keys())
+        if len(param_names) >= 1 and param_names[0] not in ("e", "event"):
+            warnings.warn(
+                UserWarning(
+                    "First handler parameter has an unusual name: '{}'".format(
+                        param_names[0]
+                    )
+                )
+            )
+        if len(param_names) >= 2 and param_names[1] not in ("c", "context"):
+            warnings.warn(
+                UserWarning(
+                    "Second handler parameter has an unusual name: '{}'".format(
+                        param_names[1]
+                    )
+                )
+            )
+
+        source = inspect.getsource(handler)
+
+        # Automatically generate description. We pluck out just the first
+        # paragraph of the docstring, formatted into a single line
+        desc = Description
+        if not desc:
+            if handler.__doc__:
+                docstring_lines = []
+                for line in handler.__doc__.split("\n"):
+                    line = line.strip()
+
+                    # Use empty lines to detect paragraph breaks, and drop
+                    # everything after the first paragraph
+                    if not line:
+                        if not docstring_lines:
+                            # ...but just ignoire leading empty lines
+                            continue
+                        break
+
+                    docstring_lines.append(line.strip())
+                desc = " ".join(docstring_lines)
+            else:
+                desc = inflection.humanize(handler.__name__) + "."
+
+        # Create a basic Function resource with only the compulsory properties
+        return cls(
+            Properties=FunctionProperties(
+                Code=dict(ZipFile=source),
+                Description=desc,
+                Handler="index.{}".format(handler.__name__),
+                Role=Role,
+                Runtime=Runtime or get_lambda_runtime_for_this_process(),
+            )
+        )
+
 
 #: Lambda Runtimes for Python that we know about.
 #: See `the definitive list in the AWS documentation
