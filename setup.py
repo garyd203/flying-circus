a5147fe9645c4791f5cdc0895501b73510a3f2af
Gary Donovan <gazza@gazza.id.au>
2019-07-11 22:27:58 +1000

    Test with Python 3.8

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Pre-processors",
         "Topic :: System :: Systems Administration",
     ],
