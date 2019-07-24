df40611b1c2742b5e4cd67623cf10081e13fb7be
Gary Donovan <gazza@gazza.id.au>
2019-07-24 23:01:47 +1000

    Update service specification to v4.3.0

---
+++ src/flyingcircus/service/iotevents.py
@@ -0,0 +1,12 @@
+"""General-use classes to interact with the IoTEvents service through CloudFormation.
+
+See Also:
+    `AWS developer guide for IoTEvents
+    <https://docs.aws.amazon.com/iotevents/latest/developerguide/index.html>`_
+"""
+
+# noinspection PyUnresolvedReferences
+from .._raw import iotevents as _raw
+
+# noinspection PyUnresolvedReferences
+from .._raw.iotevents import *
